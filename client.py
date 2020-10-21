2e3a83c64b07d3d7005852b5638d1b493801ca3e
German <gd.martinez@uniandes.edu.co>
2020-10-21 12:23:42 -0500

    UDP

---
+++ client.py
@@ -0,0 +1,83 @@
+# Import socket module
+import socket
+import logging
+import hashlib
+import time
+from _thread import *
+from threading import Thread
+import random
+
+
+def main():
+
+    cliente_num = int(random.random()*100000000000000)
+    # Initializes the client log
+    logging.basicConfig(filename="./clientsFiles/client" + str(cliente_num) + ".log", level=logging.INFO,
+                        format='%(asctime)s %(levelname)-8s %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S'
+                        )
+    print('Bienvenido')
+    m = hashlib.sha256()
+    start(m, cliente_num)
+
+
+def start(m, cliente_num):
+
+    host = socket.gethostname()
+    #host = '18.209.223.196'
+
+    # Define the port on which you want to connect
+    port = 50000
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Connect to server on local computer
+    s.sendto(("Cliente listo").encode(), (host, port))
+
+    print("Conexión con el host: ", host,
+          " fue satisfactoria en el puerto: ", port)
+    print("Cliente #", cliente_num, " listo")
+    logging.info("CLIENT: client #%d ready", cliente_num)
+    dataTotal = b''
+
+    start_time = time.time()
+    ghost = True
+    f = open("prueba.txt", 'wb')
+
+    while True:
+        # Message received from server
+        size = 1024
+        data, address = s.recvfrom(1024)
+        f.write(data)
+        dataTotal += data
+
+        if data and ghost:
+            start_time = time.time()
+            ghost = False
+        if not data:
+            break
+        elif (data.__contains__(b"HASHH")):
+            logging.info("Found hash")
+            index = data.find(b"HASHH")
+
+            index2 = dataTotal.find(b"HASHH")
+
+            m.update(dataTotal[:index2])
+
+            realM = data[index+5:]
+            print("Hash recibido ", realM.decode())
+            print("Hash creado ", m.hexdigest())
+            if m.hexdigest() == realM.decode():
+                print("Hash correcto")
+                logging.info("CLIENT: hash correcto")
+            else:
+                print("Hash incorrecto")
+                logging.info("CLIENT: hash corrupto")
+
+    logging.info('CLIENT: Cantidad bytes recibidos %s', len(dataTotal))
+    logging.info('CLIENT: Cantidad paquetes %s', len(dataTotal)/size)
+    logging.info('CLIENT: Tiempo del envío %s', (time.time()-start_time))
+    logging.info("---------------------------------------------")
+    s.close()
+
+
+main()
---
+++ server.py
@@ -0,0 +1,111 @@
+# Import socket programming library
+import socket
+
+# Import thread module
+from _thread import *
+import threading
+import logging
+import hashlib
+import time
+from threading import Thread
+
+
+# Thread function
+# Socket is the socket object with which connection was made, used to send and receive messages
+def threaded(socket, address, threadNum):
+
+    m = hashlib.sha256()
+    logging.info("SERVER thread #%s: iniciando", threadNum)
+    print("SERVER thread #", threadNum,
+          ". El archivo que se va a abrir es: ", file)
+    logging.info("SERVER thread #%s: el archivo abierto fue %s ",
+                 threadNum, file)
+    with open(file, 'rb') as f:
+
+        # File
+        data = f.read(1024)
+
+        m.update(data)
+        start_time = time.time()
+        # Send back reversed string to client
+        numBytes = socket.sendto(data, address)
+
+        print("Fin de envío thread #", threadNum)
+
+        # Send hash
+        h = m.hexdigest()
+        print("Digest enviado: ", h)
+        numBytesHash = socket.sendto(("HASHH" + h).encode(), address)
+
+        logging.info('SERVER thread #%s: bytes enviados sin hash %s',
+                     threadNum, numBytes)
+
+        logging.info('SERVER thread #%s: bytes enviados en total %s',
+                     threadNum, numBytes + numBytesHash)
+
+    logging.info('SERVER thread #%s: tiempo del envío %s', threadNum,
+                 (time.time()-start_time))
+
+
+
+def main():
+    logging.basicConfig(filename="./serverFiles/serverLog.log", level=logging.INFO,
+                        format='%(asctime)s %(levelname)-8s %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S'
+                        )
+
+    global file
+
+    # Initializes the server log
+    host = socket.gethostname()
+    # Port number
+    port = 50000
+    logging.info('Connected to %s on port %s', host, port)
+
+    # Socket del servidor
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    serversocket.bind((host, port))
+    print('Port: ', port)
+
+    # Become a server socket
+    print("Socket listening")
+    inputText = int(input("\n ¿Qué archivo desea enviar?"
+                          "\n 1. 145 MB"
+                          "\n 2. 355 MB"
+                          "\n 3. 200 MB \n"))
+    if inputText == 1:
+        f = "./data/Redes5G.mp4"
+    elif inputText == 2:
+        f = "./data/Vivaldi.mp4"
+    else:
+        f = "./data/200MB.zip"
+
+    file = f
+    print("Archivo seleccionado: ", file)
+    global num_conn
+    num_conn = int(
+        input('\n ¿A partir de cuántas conexiones desea enviar? \n'))
+    logging.info('SERVER: el número de conexiones definido es %s', num_conn)
+    print("Esperando conexiones...")
+
+    # Threads connect
+    threads = []
+    while True:
+        # Accept connections from outside
+        data, address = serversocket.recvfrom(1024)
+        # lock acquired by client
+        # print_lock.acquire()
+        print('Connected to : ', address[0], ':', address[1])
+
+        t = Thread(target=threaded, args=(serversocket, address, len(threads)))
+        threads.append(t)
+
+        if len(threads) == num_conn:
+            for i in threads:
+                i.start()
+            threads = []
+            logging.info('SERVER: reiniciando threads')
+            
+    serversocket.close()
+
+main()
