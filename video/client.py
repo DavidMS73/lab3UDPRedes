2ffbc11ab9f6118da29ab19e4e00767cdf0cad45
German <gd.martinez@uniandes.edu.co>
2020-10-23 10:33:03 -0500

    Prueba

--- video/client.py
+++ video/client.py
@@ -1,31 +1,87 @@
-import socket
-import struct
-import sys
+# import socket
+# import struct
+# import sys
 
-ack = 'ack'
-multicast_group = '18.209.223.196'
-#multicast_group = socket.gethostname()
-server_address = ('', 40000)
+# ack = 'ack'
+# multicast_group = '18.209.223.196'
+# #multicast_group = socket.gethostname()
+# server_address = ('', 40000)
 
-# Create the socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# # Create the socket
+# sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Bind to the server address
-sock.bind(server_address)
+# # Bind to the server address
+# sock.bind(server_address)
 
-# Tell the operating system to add the socket to the multicast group
-# on all interfaces.
-group = socket.inet_aton(multicast_group)
+# # Tell the operating system to add the socket to the multicast group
+# # on all interfaces.
+# group = socket.inet_aton(multicast_group)
+# mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+# sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+# # Receive/respond loop
+# while True:
+#     print('\nWaiting to receive message')
+#     data, address = sock.recvfrom(1024)
+
+#     print('Received %s bytes fromm %s' % (len(data), address))
+#     print(data)
+
+#     print('Sending acknowledgement to', address)
+#     sock.sendto(ack.encode("ascii"), address)
+
+
+
+
+# Welcome to PyShine
+# lets make the client code
+# In this code client is sending video to server
+import socket,cv2, pickle,struct
+import pyshine as ps # pip install pyshine
+import imutils # pip install imutils
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+host_ip = '18.209.223.196' # Here according to your server ip write the address
+port = 9999
+#Multicast stuff
+channel = int(input("Ingrese el canal a conectarse (1, 2 o 3)"))
+multicast_groups = ['18.209.223.196', '18.209.223.196', '18.209.223.196']
+server_address = ('', 10000)
+
+#Bind para multicast
+client_socket.connect((host_ip,port))
+
+group = socket.inet_aton(multicast_groups[channel-1])
 mreq = struct.pack('4sL', group, socket.INADDR_ANY)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+#client_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
-# Receive/respond loop
-while True:
-    print('\nWaiting to receive message')
-    data, address = sock.recvfrom(1024)
+try:
+    print('CLIENT {} CONNECTED!'.format(20))
+    if client_socket:  # if a client socket exists
+        data = b""
+        payload_size = struct.calcsize("Q")
+        while True:
+            while len(data) < payload_size:
+                packet = client_socket.recv(4 * 1024)  # 4K
+                if not packet: break
+                data += packet
+            packed_msg_size = data[:payload_size]
+            data = data[payload_size:]
+            msg_size = struct.unpack("Q", packed_msg_size)[0]
 
-    print('Received %s bytes fromm %s' % (len(data), address))
-    print(data)
-
-    print('Sending acknowledgement to', address)
-    sock.sendto(ack.encode("ascii"), address)
+            while len(data) < msg_size:
+                data += client_socket.recv(4 * 1024)
+            frame_data = data[:msg_size]
+            data = data[msg_size:]
+            frame = pickle.loads(frame_data)
+            frame = ps.putBText(frame, f"Canal {channel}", 10, 10, vspace=10, hspace=1, font_scale=0.7,
+                                 text_RGB=(255, 250, 250))
+            cv2.imshow(f"FROM ", frame)
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('q'):
+                break
+        client_socket.close()
+except Exception as e:
+    print(e)
+    client_socket.close()
+    print(f"CLIENT DISCONNECTED")
+    pass
--- video/server.py
+++ video/server.py
@@ -1,41 +1,96 @@
-import socket
-import struct
-import sys
+# import socket
+# import struct
+# import sys
 
-message = 'message in multicast'
-multicast_group = ('18.209.223.196', 40000)
-#multicast_group = (socket.gethostname(), 40000)
+# message = 'message in multicast'
+# multicast_group = ('18.209.223.196', 40000)
+# #multicast_group = (socket.gethostname(), 40000)
 
-# Create the datagram socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# # Create the datagram socket
+# sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Set a timeout so the socket does not block indefinitely when trying
-# to receive data.
-sock.settimeout(20)
+# # Set a timeout so the socket does not block indefinitely when trying
+# # to receive data.
+# sock.settimeout(20)
 
-# Set the time-to-live for messages to 1 to they do not go past the
-# local network segment.
+# # Set the time-to-live for messages to 1 to they do not go past the
+# # local network segment.
+# ttl = struct.pack('b', 1)
+# sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+
+
+# try:
+
+#     # Send data to the multicast group
+#     print('Enviando "%s"' % message)
+
+#     sent = sock.sendto(message.encode("ascii"), multicast_group)
+
+#     while True:
+#         print('Esperando respuesta')
+#         try:
+#             data, server = sock.recvfrom(16)
+#         except socket.timeout:
+#             print('timed out, no se recibieron mas respuestas')
+#             break
+#         else:
+#             print('Recibido "%s" de %s' % (data, server))
+
+# finally:
+#     print('Socket cerrado')
+#     sock.close()
+
+
+
+
+# Welcome to PyShine
+# In this video server is receiving video from clients.
+# Lets import the libraries
+import socket, cv2, pickle, struct
+import imutils
+import threading
+import pyshine as ps  # pip install pyshine
+import cv2
+vid = cv2.VideoCapture('../data/test.mp4')
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#Multicast Stuff
+multicast_group = ('18.209.223.196', 10000)
 ttl = struct.pack('b', 1)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+#server_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+
+host_name = socket.gethostname()
+host_ip = socket.gethostbyname(host_name)
+print('HOST IP:', host_ip)
+port = 9999
+socket_address = (host_ip, port)
+server_socket.bind(socket_address)
+server_socket.listen()
+print("Listening at", socket_address)
 
 
-try:
+def show_client(addr, client_socket):
+    if client_socket:
+        while (vid.isOpened()):
+            try:
+                img, frame = vid.read()
+                frame = imutils.resize(frame, width=380)
+                a = pickle.dumps(frame)
+                message = struct.pack("Q", len(a)) + a
+                client_socket.sendto(message,multicast_group)
+                key = cv2.waitKey(1) & 0xFF
+                if key == ord("q"):
+                    client_socket.close()
+            except Exception as e:
+                client_socket.close()
+                print(f'VIDEO FINISHED! to client {addr}')
+                break
 
-    # Send data to the multicast group
-    print('Enviando "%s"' % message)
 
-    sent = sock.sendto(message.encode("ascii"), multicast_group)
 
-    while True:
-        print('Esperando respuesta')
-        try:
-            data, server = sock.recvfrom(16)
-        except socket.timeout:
-            print('timed out, no se recibieron mas respuestas')
-            break
-        else:
-            print('Recibido "%s" de %s' % (data, server))
-
-finally:
-    print('Socket cerrado')
-    sock.close()
+while True:
+    client_socket, addr = server_socket.accept()
+    thread = threading.Thread(target=show_client, args=(addr, client_socket))
+    thread.start()
+    print("TOTAL CLIENTS ", threading.activeCount() - 1)
